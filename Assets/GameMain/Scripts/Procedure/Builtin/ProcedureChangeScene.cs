16ede30cdb8fa6c9609467e78ecb53d98c972171
环镜分身 <1934036260@qq.com>
2020-06-21 14:17:03 +0800

    2020年6月21日14:16:30
    第一提交

---
+++ Assets/GameMain/Scripts/Procedure/Builtin/ProcedureChangeScene.cs
@@ -0,0 +1,155 @@
+﻿using GameFramework;
+using GameFramework.DataTable;
+using GameFramework.Event;
+using GameFramework.Procedure;
+using UnityGameFramework.Runtime;
+using ProcedureOwner = GameFramework.Fsm.IFsm<GameFramework.Procedure.IProcedureManager>;
+
+namespace FlappyBird
+{
+
+    /// <summary>
+    /// 切换场景流程
+    /// </summary>
+    public partial class ProcedureChangeScene : ProcedureBase
+    {
+
+        /// <summary>
+        /// 是否完成切换场景
+        /// </summary>
+        private bool m_IsChangeSceneComplete = false;
+        private int m_BackgroundMusicId = 0;
+        private int gotoSceneId = 0;
+
+        protected override void OnEnter(ProcedureOwner procedureOwner)
+        {
+            base.OnEnter(procedureOwner);
+
+            m_IsChangeSceneComplete = false;
+
+            // 1 订阅事件
+            GameEntry.Event.Subscribe(LoadSceneSuccessEventArgs.EventId, OnLoadSceneSuccess);
+            GameEntry.Event.Subscribe(LoadSceneFailureEventArgs.EventId, OnLoadSceneFailure);
+            GameEntry.Event.Subscribe(LoadSceneUpdateEventArgs.EventId, OnLoadSceneUpdate);
+            GameEntry.Event.Subscribe(LoadSceneDependencyAssetEventArgs.EventId, OnLoadSceneDependencyAsset);
+
+            // 2 停止所有声音
+            GameEntry.Sound.StopAllLoadingSounds();
+            GameEntry.Sound.StopAllLoadedSounds();
+
+            // 3 隐藏所有实体
+            GameEntry.Entity.HideAllLoadingEntities();
+            GameEntry.Entity.HideAllLoadedEntities();
+
+            // 4 卸载所有场景
+            string[] loadedSceneAssetNames = GameEntry.Scene.GetLoadedSceneAssetNames();
+            for (int i = 0; i < loadedSceneAssetNames.Length; i++)
+            {
+                GameEntry.Scene.UnloadScene(loadedSceneAssetNames[i]);
+            }
+
+            // 5 还原游戏速度
+            GameEntry.Base.ResetNormalGameSpeed();
+
+            // 6 读取流程状态机中的数据
+            gotoSceneId = procedureOwner.GetData<VarInt>(Constant.ProcedureData.NextSceneId).Value;//1 获取要切换的场景id
+            IDataTable<DRScene> dtScene = GameEntry.DataTable.GetDataTable<DRScene>();//2 获取数据表中的所有行
+            DRScene drScene = dtScene.GetDataRow(gotoSceneId);//3 根据场景id筛选出目标行对应的场景
+            
+            if (drScene == null)//如果选出的场景是空 退出函数
+            {
+                Log.Warning("Can not load scene '{0}' from data table.", gotoSceneId.ToString());
+                return;
+            }
+
+            GameEntry.Scene.LoadScene(AssetUtility.GetSceneAsset(drScene.AssetName), this);//加载场景
+            m_BackgroundMusicId = drScene.BackgroundMusicId;//设置音乐id
+        }
+
+        protected override void OnLeave(ProcedureOwner procedureOwner, bool isShutdown)
+        {
+            GameEntry.Event.Unsubscribe(LoadSceneSuccessEventArgs.EventId, OnLoadSceneSuccess);
+            GameEntry.Event.Unsubscribe(LoadSceneFailureEventArgs.EventId, OnLoadSceneFailure);
+            GameEntry.Event.Unsubscribe(LoadSceneUpdateEventArgs.EventId, OnLoadSceneUpdate);
+            GameEntry.Event.Unsubscribe(LoadSceneDependencyAssetEventArgs.EventId, OnLoadSceneDependencyAsset);
+
+            base.OnLeave(procedureOwner, isShutdown);
+        }
+
+        protected override void OnUpdate(ProcedureOwner procedureOwner, float elapseSeconds, float realElapseSeconds)
+        {
+            base.OnUpdate(procedureOwner, elapseSeconds, realElapseSeconds);
+
+            if (!m_IsChangeSceneComplete)
+            {
+                return;
+            }
+
+            //TODO：在这里根据切换到的场景编号进行对应的流程切换
+            switch (gotoSceneId)
+            {
+                case 1:
+                    ChangeState<ProcedureMenu>(procedureOwner);
+                    break;
+
+                case 2:
+                    ChangeState<ProcedureMain>(procedureOwner);
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        private void OnLoadSceneSuccess(object sender, GameEventArgs e)
+        {
+            LoadSceneSuccessEventArgs ne = (LoadSceneSuccessEventArgs)e;
+            if (ne.UserData != this)
+            {
+                return;
+            }
+
+            Log.Info("Load scene '{0}' OK.", ne.SceneAssetName);
+
+            if (m_BackgroundMusicId > 0)
+            {
+                GameEntry.Sound.PlayMusic(m_BackgroundMusicId);
+            }
+
+            m_IsChangeSceneComplete = true;
+        }
+
+        private void OnLoadSceneFailure(object sender, GameEventArgs e)
+        {
+            LoadSceneFailureEventArgs ne = (LoadSceneFailureEventArgs)e;
+            if (ne.UserData != this)
+            {
+                return;
+            }
+
+            Log.Error("Load scene '{0}' failure, error message '{1}'.", ne.SceneAssetName, ne.ErrorMessage);
+        }
+
+        private void OnLoadSceneUpdate(object sender, GameEventArgs e)
+        {
+            LoadSceneUpdateEventArgs ne = (LoadSceneUpdateEventArgs)e;
+            if (ne.UserData != this)
+            {
+                return;
+            }
+
+            Log.Info("Load scene '{0}' update, progress '{1}'.", ne.SceneAssetName, ne.Progress.ToString("P2"));
+        }
+
+        private void OnLoadSceneDependencyAsset(object sender, GameEventArgs e)
+        {
+            LoadSceneDependencyAssetEventArgs ne = (LoadSceneDependencyAssetEventArgs)e;
+            if (ne.UserData != this)
+            {
+                return;
+            }
+
+            Log.Info("Load scene '{0}' dependency asset '{1}', count '{2}/{3}'.", ne.SceneAssetName, ne.DependencyAssetName, ne.LoadedCount.ToString(), ne.TotalCount.ToString());
+        }
+    }
+}
---
+++ Assets/GameMain/Scripts/UI/Customs/SettingForm.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FlappyBird
+{
+    /// <summary>
+    /// 设置界面脚本
+    /// </summary>
+    public class SettingForm : UGuiForm
+    {
+        public void OnCloseButtonClick()
+        {
+            Close();
+        }
+
+        public void OnMusicSettingValueChange(float value)
+        {
+            GameEntry.Sound.SetVolume("Music", value);
+        }
+
+        public void OnSoundSettingValueChange(float value)
+        {
+            GameEntry.Sound.SetVolume("Sound", value);
+            GameEntry.Sound.SetVolume("UISound", value);
+        }
+    }
+}
